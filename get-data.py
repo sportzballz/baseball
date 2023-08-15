9eaae42c1f6ea402a0d51567b740bd522de65196
asmith986 <adam_smith3@comcast.com>
2023-08-15 14:49:21 -0400

    initial commit

---
+++ get-data.py
@@ -0,0 +1,9 @@
+import statsapi
+import json
+
+def main():
+    #data = statsapi.schedule(start_date='08/15/2023',end_date='08/15/2023')
+    data = statsapi.lookup_team('chi')
+    # statsapi.meta(type, fields=None)
+
+    print(json.dumps(data, indent=2))
