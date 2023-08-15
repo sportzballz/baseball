fb750cef649448762d9730fbe5db9cbf0707e2af
asmith986 <adam_smith3@comcast.com>
2023-08-15 15:17:05 -0400

    initial

--- get-data.py
+++ get-data.py
@@ -1,8 +1,9 @@
 import statsapi
 import json
 
+
 def main():
-    #data = statsapi.schedule(start_date='08/15/2023',end_date='08/15/2023')
+    # data = statsapi.schedule(start_date='08/15/2023',end_date='08/15/2023')
     data = statsapi.lookup_team('chi')
     # statsapi.meta(type, fields=None)
 
