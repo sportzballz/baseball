92471d34dba8b4fd3aca14879ab9c0815a493aeb
asmith986 <adam_smith3@comcast.com>
2023-08-18 13:00:26 -0400

    refactor

--- src/connector/slack.py
+++ src/connector/slack.py
@@ -2,11 +2,8 @@ import os
 
 from slack_sdk import WebClient
 
-from slack_sdk.errors import SlackApiError
-
 
 def post(msg: str):
-    client = WebClient(token=os.environ.get("SLACK_BOT_TOKEN"))
-
+    client = WebClient(token=os.environ['SLACK_TOKEN'])
     client.chat_postMessage(channel="#sportzballz", text=msg)
 
