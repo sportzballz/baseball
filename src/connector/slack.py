12af7d689e803aff9cbcc533a9a92cb91fb1d150
asmith986 <adam_smith3@comcast.com>
2023-08-18 13:27:01 -0400

    refactor

--- src/connector/slack.py
+++ src/connector/slack.py
@@ -5,5 +5,5 @@ from slack_sdk import WebClient
 
 def post(msg: str):
     client = WebClient(token=os.environ['SLACK_TOKEN'])
-    client.chat_postMessage(channel="#sportzballz", text=msg)
+    client.chat_postMessage(channel="#sportzballz", text='@here'+msg)
 
