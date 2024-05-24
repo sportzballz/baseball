7e0f8ef5bcd370172eb129f102821b8a77ee2b43
asmith986 <adam_smith3@comcast.com>
2024-05-24 12:22:39 -0400

    Add slack token to backtest

--- src/connector/slack.py
+++ src/connector/slack.py
@@ -3,11 +3,11 @@ from slack_sdk import WebClient
 
 
 def post_backtest(msg: str):
-    client = WebClient(token=os.environ['SLACK_TOKEN'])
+    client = WebClient(token=os.environ['SLACK_USER_TOKEN'])
     client.chat_postMessage(channel="#sportzballz-backtest", text=msg, icon_emoji=':sportzballz:', username='SportzBallz')
 
 
 def post(msg: str):
-    client = WebClient(token=os.environ['SLACK_TOKEN'])
+    client = WebClient(token=os.environ['SLACK_USER_TOKEN'])
     client.chat_postMessage(channel="#sportzballz", text=msg, icon_emoji=':baseball:', username='SportzBallz')
 
