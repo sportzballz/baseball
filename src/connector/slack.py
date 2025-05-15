431e61d96c180b054a8139d131ab69ce09496b8d
Smith, Adam <ADAM_SMITH3@comcast.com>
2025-05-15 17:42:22 -0400

    Post to sportzballz.slack.com workspace

--- src/connector/slack.py
+++ src/connector/slack.py
@@ -32,7 +32,15 @@ def post(msg: str, model: str):
         print(f'Posting #todays-picks')
         client.chat_postMessage(channel=f"#todays-picks", text=msg, icon_emoji=':sportzballz:', username='SportzBallz')
 
-
+def post_sportzballz(msg: str):
+    client = WebClient(token=os.environ['SPORTZBALLZ_SLACK_TOKEN'])
+    client.chat_postMessage(channel=f"#todays-picks", text=msg, icon_emoji=':sportzballz:', username='sportzballz')
+    hour = datetime.now(pytz.timezone('US/Eastern')).strftime("%H")
+    # post todays-picks at 5pm
+    print(f'Hour is: {hour}')
+    if hour == "12":
+        print(f'Posting #daily-results')
+        client.chat_postMessage(channel=f"#daily-results", text=msg, icon_emoji=':sportzballz:', username='SportzBallz')
 
 def is_already_posted(target_channel: str):
     client = WebClient(token=os.environ['SLACK_TOKEN'])
